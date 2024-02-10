52fd7ed6b223cc3b1dc2d7f6495096f674cddd37
Brunofell <brunofelimarti@gmail.com>
2024-02-10 19:10:00 -0300

    mensagem

---
+++ README.md
@@ -0,0 +1,3 @@
+# cookin-up 
+
+Primeiro projeto em VUE
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+export default{ // se a prorpiedade é igual ao seu valor, pode deixa um nome só
+  components: { Banner, ConteudoPrincipal}
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
+
